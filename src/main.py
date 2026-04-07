3472b9506aad8907edd4d890151f4d3796f26a64
BerdanKoc <berdan.koc@ecoles-epsi.net>
2026-04-07 17:31:30 +0200

    feat: ajout de la statistique de niveau

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 class Personnage:
-    def __init__(self, nom, hp=10):
+    def __init__(self, nom, niveau=0):
         self.nom = nom
-        self.hp = hp
+        self.niveau = niveau
+        self.hp = 10 + (self.niveau * 2)
 
     @property
     def est_vivant(self):
         return self.hp > 0
 
     def attaquer(self, cible):
-        cible.recevoir_degats(1)
+        degats = 1 + (self.niveau * 2)
+        cible.recevoir_degats(degats)
 
     def recevoir_degats(self, degats):
         self.hp -= degats
