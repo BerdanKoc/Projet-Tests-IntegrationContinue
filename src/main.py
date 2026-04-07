441aa3530319ce1466d3e3750832cbe77e21cb20
BerdanKoc <berdan.koc@ecoles-epsi.net>
2026-04-07 17:28:03 +0200

    feat: les degats dependent de la force

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 class Personnage:
-    def __init__(self, nom, hp=10):
+    def __init__(self, nom, force=0):
         self.nom = nom
-        self.hp = hp
+        self.force = force
+        self.hp = 10
 
     @property
     def est_vivant(self):
         return self.hp > 0
 
     def attaquer(self, cible):
-        cible.recevoir_degats(1)
+        degats = 1 + self.force
+        cible.recevoir_degats(degats)
 
     def recevoir_degats(self, degats):
         self.hp -= degats
