8fcdff99ade9b72b207f6ffbc4ed02c4f57f55c3
BerdanKoc <berdan.koc@ecoles-epsi.net>
2026-04-07 15:30:25 +0200

    feat: initialisation du RPG avec TDD et Gherkin

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+class Personnage:
+    def __init__(self, nom, hp=10):
+        self.nom = nom
+        self.hp = hp
+
+    @property
+    def est_vivant(self):
+        return self.hp > 0
+
+    def attaquer(self, cible):
+        cible.recevoir_degats(1)
+
+    def recevoir_degats(self, degats):
+        self.hp -= degats
+        if self.hp < 0:
+            self.hp = 0
